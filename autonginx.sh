3addd68c2e2d0ce0dd9046fce08773ae2949fd3c
MojitoTea <pixelonlline@gmail.com>
2022-10-10 01:37:45 +0300

    Changed Default Nginx page

--- autonginx.sh
+++ autonginx.sh
@@ -7,3 +7,10 @@ else
   sudo apt install nginx -y
   echo "Nginx Installed..."
 fi
+nginx_page="/var/www/html/index.nginx-debian.html"
+sudo chmod 777 "$nginx_page"
+IP=$(hostname -I)
+dat=$(date +%Y-%m-%d)
+echo "$IP"
+echo "$Changed"
+echo "Hello World NGINX  $IP $dat " > "$nginx_page"
